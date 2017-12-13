9d71bc06a4ff742be949bcae0c38ed10995cc621
Pål Brattberg <pal@subtree.se>
2017-12-13 15:21:44 +0100

    Handle the basic cases

--- romanNumerals-tests.js
+++ romanNumerals-tests.js
@@ -2,10 +2,20 @@ var RomanNumerals = require('./romanNumerals')
 
 var roman = new RomanNumerals()
 
+let counter = 0
 function ator (a, r) {
+  counter++
   let romNum = roman.ator(a)
   if (r !== romNum) throw new Error('Expected ' + r + ' from ' + a + ' but got ' + romNum)
 }
 
+ator(0, 'nulla')
+ator(1, 'I')
 ator(5, 'V')
-// ator(4, 'IV')
+ator(10, 'X')
+ator(50, 'L')
+ator(100, 'C')
+ator(500, 'D')
+ator(1000, 'M')
+
+console.log('Good job! ' + counter + ' tests ran successfully!')
--- romanNumerals.js
+++ romanNumerals.js
@@ -3,7 +3,16 @@ function romanNumerals () {
    * Arabic to Roman
    */
   this.ator = function (arabic) {
-    return 'V'
+    switch (arabic) {
+      case 1: return 'I'
+      case 5: return 'V'
+      case 10: return 'X'
+      case 50: return 'L'
+      case 100: return 'C'
+      case 500: return 'D'
+      case 1000: return 'M'
+    }
+    return 'nulla'
   }
 
   /**
